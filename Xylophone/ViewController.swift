b802c59d405ba11cb058c325344249f9302f5928
Adwait Barkale <adwaitb@Shrutis-Mac-mini.local>
2020-07-23 22:54:25 +0530

    First Commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        //print(sender.currentTitle)
+        
+        let soundName = sender.currentTitle!
+       // print(soundName)
+        sender.alpha = 0.5
+        print("Start \(soundName)")
+        playSound(soundName)
+        sleep(UInt32(0.2))
+        sender.alpha = 1
+        print("End")
+        
+    }
+    
+    func playSound(_ soundName: String)    {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
 }
+
 
